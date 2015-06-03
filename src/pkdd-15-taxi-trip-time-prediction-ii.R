8f72813e2904a567b4a9f6d540b28e2b38a7ade4
debugger87 <yangchaozhong.2009@gmail.com>
2015-06-03 15:42:35 +0800

    first commit

---
+++ src/pkdd-15-taxi-trip-time-prediction-ii.R
@@ -0,0 +1,14 @@
+library(readr)
+library(rjson)
+
+test <- read_csv("input/test.csv")
+mean_train_time <- 660
+positions <- function(row) as.data.frame(do.call(rbind, fromJSON(row$POLYLINE)))
+
+submission <- test["TRIP_ID"]
+
+for (i in 1:nrow(test)) {
+  submission$TRAVEL_TIME[i] <- max(15*nrow(positions(test[i,])), mean_train_time)
+}
+
+write_csv(submission, "my_submission.csv")
